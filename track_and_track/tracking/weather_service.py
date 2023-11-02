45d6ce02d778ca99ee529fe4849177a238c57c6d
ario <cybera.3s@gmail.com>
2023-11-02 10:49:07 +0330

    feat: add get_weather_data func

---
+++ track_and_track/tracking/weather_service.py
@@ -0,0 +1,15 @@
+import requests
+from django.conf import settings
+
+
+def get_weather_data(city_name) -> dict:
+    """
+    Get weather data by city name from openweather API
+    :param city_name: the city to get its weather data
+    :return: dict of weather data
+    """
+    api_key: str = settings.OPEN_WEATHER_API_KEY
+    url: str = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}"
+
+    response = requests.get(url)
+    return response.json()
