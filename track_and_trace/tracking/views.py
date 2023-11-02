5f9e6a7429cab1cd3d7994728bb070a893c261cd
ario <cybera.3s@gmail.com>
2023-11-02 12:24:46 +0330

    doc: refactor ShipmentView

--- track_and_trace/tracking/views.py
+++ track_and_trace/tracking/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
+from rest_framework.response import Response
 
 # local imports
 from .serializers import ShipmentSerializer
@@ -14,6 +15,16 @@ class ShipmentView(generics.ListAPIView):
 
     queryset = Shipment.objects.all()
     serializer_class = ShipmentSerializer
-    lookup_field = "tracking_number"
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["tracking_number", "carrier"]
+
+    def list(self, request, *args, **kwargs):
+
+        # Make query params required
+        tracking_number = self.request.query_params.get('tracking_number', None)
+        carrier = self.request.query_params.get('carrier', None)
+        if not (tracking_number or carrier):
+            return Response([])
+
+        return super().list(request, *args, **kwargs)
+
